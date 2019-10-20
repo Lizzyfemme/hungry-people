80f82759bfe66aec61af2ce588c35aa3ee37a222
Fabio Papa <fabtheman@gmail.com>
2019-10-19 20:10:47 -0700

    Automatically calculate subtotal for new order page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,24 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  const $quantityInputs = $('.qty');
+  $quantityInputs.change((el) => {
+    updateTotal();
+  });
+  // $quantityInputs.each(function (index) {
+  //   $(this).siblings(".menu_item_price").value;
+  // });
 });
+
+const updateTotal = () => {
+  const $subTotal = $('#sub-total');
+  let subTotal = 0.00;
+  const $quantityInputs = $('.qty');
+
+  $quantityInputs.each(function (index) {
+    const qty = Number.parseInt(this.value);
+    const price = $(this).siblings(".menu_item_price")[0].value.slice(1);
+
+    subTotal += qty * price;
+  });
+
+  $subTotal.text(`$${subTotal.toFixed(2)}`);
+};
