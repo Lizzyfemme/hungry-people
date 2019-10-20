8fb05b8f972de7aa03b18d8cd871206aec6f873e
Jacky Lau <jackylau0126@gmail.com>
2019-10-20 00:08:14 +0000

    add seeds

---
+++ db/seeds/01_menu_items.sql
@@ -0,0 +1,11 @@
+INSERT INTO menu_items (menu_item_name, description, price)
+VALUES
+('Margherita', 'Fior di Latte, Fresh Basil, Maldon Salt, Marinara', 17.50),
+('Finocchiona', 'Fennel Sausage, Caramelized Onions, Green Olives, Smoked Provolone, Marinara', 20.00),
+('Funghi', 'Roasted Mushrooms, Goat Cheese, Arugula, Pecorino Tartufo, Bianco', 20.00),
+('Pesto & Ricotta', 'Basil Pesto, Fresh Ricotta, Pine Nuts, Parmigiano, Bianco', 18.00),
+('Carbonara', 'Pecorino, Pancetta, Farm Egg, Bianco', 19.00),
+('Pineapple & Ham', 'House-Smoked Ham, Roasted Pineapple, Confit Garlic, Rosemary, manchego, bianco', 19.00),
+('Artichoke & Olive', 'Fior di Latte, Kalamata Olives, Artichokes, Garlic Confit, Basil, Marinara', 17.00),
+('Quattro Formaggio', 'Taleggio, Fresh Ricotta, Mozzarella, Parmigiano, Marinara', 18.00),
+('Pepperoni', 'Thinly-Sliced Pepperoni, Mozzarella, Parmigiano, Oregano, Marinara', 18.00);
---
+++ db/seeds/03_line_items.sql
@@ -0,0 +1,6 @@
+INSERT INTO line_items(menu_item_id, order_id, quantity)
+VALUES
+(1, 1, 1),
+(4, 2, 2),
+(6, 3, 1),
+(5, 3, 1);
