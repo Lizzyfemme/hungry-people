3ce17c4ec35c3b736db096d560ab0879c1a72b62
Fabio Papa <fabtheman@gmail.com>
2019-10-20 13:35:29 -0700

    Added ability to submit an order
    - Order and line items get added to DB
    - Customer receives text message
    - Restaurant receives text message
    - Redirect to checkout

--- routes/orders.js
+++ routes/orders.js
@@ -9,6 +9,62 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db, twilioClient) => {
+  router.post("/", (req, res) => {
+    const customerPhone = req.params.customer_phone;
+    const orderedAt = new Date();
+    const values = [customerPhone, orderedAt];
+
+    db.query(`
+    INSERT INTO orders (customer_phone, ordered_at)
+    VALUES ('${customerPhone}', '${orderedAt.toISOString()}')
+    RETURNING *;
+    `)
+      .then(orderData => {
+        const idParams = req.body.menu_item_id;
+        const qtyParams = req.body.menu_item_quantity;
+        let lineItemRows = [];
+
+        for (let i = 0; i < idParams.length; i ++) {
+          if (qtyParams[i] > 0) {
+            lineItemRows.push(`(${idParams[i]}, ${orderData.rows[0].id}, ${qtyParams[i]})`);
+          }
+        }
+
+        return db.query(`
+        INSERT INTO line_items (menu_item_id, order_id, quantity)
+        VALUES ${lineItemRows.join(', ')};
+        `);
+      })
+      .then(() => {
+        return twilioClient.messages.create({
+          to: req.body.customer_phone,
+          from: '+17609708429',
+          body: `
+Thank you for placing an order with Bufala!
+We'll send you a text shortly to let you know how long your order will take.
+`
+        });
+      })
+      .then(() => {
+        return twilioClient.messages.create({
+          to: '7782305559',
+          from: '+17609708429',
+          body: `
+A customer has placed an order!
+Go to your orders dashboard to let the customer know how long their order will be.
+`
+        });
+      })
+      .then(() => {
+        res.redirect('/orders/checkout');
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
   router.get("/new", (req, res) => {
     db.query(`SELECT id, menu_item_name, description, photo_url, price FROM menu_items;`)
       .then(data => {
