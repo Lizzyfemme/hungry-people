c616cf1525ba5dfcf47e92102a4c148f9facd253
Fabio Papa <fabtheman@gmail.com>
2019-10-21 11:30:22 -0700

    Fix a bug with the sub-total no longer being calculated

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,8 +12,8 @@ const updateTotal = () => {
 
   $quantityInputs.each(function (index) {
     const qty = Number.parseInt(this.value);
-    const menuItemEl = $(this).parentsUntil("article").parent();
-    const price = menuItemEl.children(".menu_item_price")[0].value.slice(1);
+    const menuItemEl = $(this).parentsUntil("article").parent()[0];
+    const price = $(menuItemEl).find(".menu_item_price")[0].value.slice(1);
 
     subTotal += qty * price;
   });
