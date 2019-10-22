5ffd25b26e89162c5f71231509b04d59a593d8bf
Fabio Papa <fabtheman@gmail.com>
2019-10-22 16:08:08 -0700

    Fix a bug introduced to SQL query

--- routes/orders.js
+++ routes/orders.js
@@ -89,8 +89,7 @@ http://localhost:8080/restaurant/orders/${orderID}
     let menu_item;
 
     db.query(`
-    SELECT orders.id, orders.customer_phone, string_agg(CONCAT (line_items.quantity, ' ', menu_items.menu_item_name), ', ')
-
+    SELECT menu_items.menu_item_name, menu_items.price, line_items.quantity
     FROM line_items
     JOIN orders ON orders.id = order_id
     JOIN menu_items ON menu_items.id = line_items.menu_item_id
