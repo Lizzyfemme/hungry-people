91cab427c9f7d55717b7008c8c613350cd134e4a
Fabio Papa <fabtheman@gmail.com>
2019-10-23 07:48:13 -0700

    Add prep time column to orders table, update seeds

--- db/schema/02_orders.sql
+++ db/schema/02_orders.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS orders CASCADE;
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   customer_phone VARCHAR(20),
+  prep_time SMALLINT,
   ordered_at TIMESTAMP NOT NULL,
   received_at TIMESTAMP,
   completed_at TIMESTAMP
